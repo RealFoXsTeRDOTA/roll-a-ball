e42536c0439a7670277a852f0d9fe9287c54a805
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-02-20 13:29:17 +0100

    Add obstacle course and stopwatch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,37 +1,61 @@
+using System.Diagnostics;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-  public float MoveSpeed = 1.0f;
+  public float MoveSpeed = 10.0f;
+  public float JumpForce = 5.0f;
   public TextMeshProUGUI CountText;
   public GameObject WinText;
+  public TextMeshProUGUI TimeText;
 
   private Rigidbody body;
+  private SphereCollider sphereCollider;
   private float movementX;
-  private float movementY;
+  private float movementZ;
   private int count;
+  private int totalCount = 13;
+  private bool inputsEnabled;
+  private Stopwatch stopwatch;
 
   private void Start()
   {
     body = GetComponent<Rigidbody>();
+    sphereCollider = GetComponent<SphereCollider>();
     count = 0;
     SetCountText();
     WinText.SetActive(false);
+    inputsEnabled = true;
+    stopwatch = Stopwatch.StartNew();
   }
 
   private void FixedUpdate()
   {
-    var movement = new Vector3(movementX, 0.0f, movementY);
-    body.AddForce(movement * MoveSpeed);
+    if (inputsEnabled)
+    {
+      var movement = new Vector3(movementX, 0.0f, movementZ);
+      body.AddForce(movement * MoveSpeed);
+    }
   }
 
   private void OnMove(InputValue inputValue)
   {
-    var movement = inputValue.Get<Vector2>();
-    movementX = movement.x;
-    movementY = movement.y;
+    if (inputsEnabled)
+    {
+      var movement = inputValue.Get<Vector2>();
+      movementX = movement.x;
+      movementZ = movement.y;
+    }
+  }
+
+  private void OnJump()
+  {
+    if (inputsEnabled && IsGrounded())
+    {
+      body.velocity = new Vector3(body.velocity.x, 1.0f * JumpForce, body.velocity.z);
+    }
   }
 
   private void OnTriggerEnter(Collider other)
@@ -46,11 +70,20 @@ public class PlayerController : MonoBehaviour
 
   private void SetCountText()
   {
-    CountText.text = count.ToString();
+    CountText.text = $"{count}/{totalCount}";
 
-    if (count >= 13)
+    if (count >= totalCount)
     {
+      stopwatch.Stop();
+      TimeText.SetText($"Time spent: {stopwatch.Elapsed}");
       WinText.SetActive(true);
+      inputsEnabled = false;
     }
+  }
+
+  private bool IsGrounded()
+  {
+    var extraHeight = 0.3f;
+    return Physics.Raycast(sphereCollider.bounds.center, Vector3.down, sphereCollider.bounds.extents.y + extraHeight);
   }
 }
